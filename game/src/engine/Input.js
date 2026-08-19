5dd59e3c56655e99cac3578d6cd1528123537cbd
nhicksenterprises2025-maker <nhicks.enterprises2025@gmail.com>
2026-08-19 18:50:15 -0400

    Add mouse aim and sprint input

--- game/src/engine/Input.js
+++ game/src/engine/Input.js
@@ -1,21 +1,38 @@
-const BLOCKED_KEYS = new Set(['KeyW', 'KeyA', 'KeyS', 'KeyD', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Tab', 'F1']);
+const BLOCKED_KEYS = new Set([
+  'KeyW', 'KeyA', 'KeyS', 'KeyD',
+  'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight',
+  'ShiftLeft', 'ShiftRight', 'Tab', 'F1'
+]);
 
 export class Input {
   constructor(target = window) {
     this.down = new Set();
     this.pressed = new Set();
+    this.pointer = { x: innerWidth / 2, y: innerHeight / 2, inside: true };
+
     this.onKeyDown = (event) => {
       if (BLOCKED_KEYS.has(event.code)) event.preventDefault();
       if (!this.down.has(event.code)) this.pressed.add(event.code);
       this.down.add(event.code);
     };
     this.onKeyUp = (event) => this.down.delete(event.code);
+    this.onPointerMove = (event) => {
+      this.pointer.x = event.clientX;
+      this.pointer.y = event.clientY;
+      this.pointer.inside = true;
+    };
+    this.onPointerLeave = () => {
+      this.pointer.inside = false;
+    };
     this.onBlur = () => {
       this.down.clear();
       this.pressed.clear();
     };
+
     target.addEventListener('keydown', this.onKeyDown);
     target.addEventListener('keyup', this.onKeyUp);
+    target.addEventListener('pointermove', this.onPointerMove);
+    target.addEventListener('pointerleave', this.onPointerLeave);
     target.addEventListener('blur', this.onBlur);
   }
 
@@ -40,6 +57,14 @@ export class Input {
       y *= inv;
     }
     return { x, y };
+  }
+
+  sprintHeld() {
+    return this.isDown('ShiftLeft', 'ShiftRight');
+  }
+
+  pointerPosition() {
+    return { ...this.pointer };
   }
 
   endFrame() {
