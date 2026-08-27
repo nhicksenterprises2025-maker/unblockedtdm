b7b199c567b281332a4204964af60fb49c70b13a
nhicksenterprises2025-maker <nhicks.enterprises2025@gmail.com>
2026-08-27 12:37:25 -0400

    Expose packaged UI ready signal

--- game/src/preload.js
+++ game/src/preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('gameAPI', {
   getBuildInfo: () => ipcRenderer.invoke('game:get-build-info'),
   toggleFullscreen: () => ipcRenderer.invoke('game:toggle-fullscreen'),
-  quit: () => ipcRenderer.invoke('game:quit')
+  quit: () => ipcRenderer.invoke('game:quit'),
+  uiReady: () => ipcRenderer.send('game:ui-ready')
 });
